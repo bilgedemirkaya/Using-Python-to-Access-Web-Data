38c709bc3feb8f9a5dcb434049ac2adb11d9ef2b
bilgedemirkaya <59152610+bilgedemirkaya@users.noreply.github.com>
2020-11-12 11:28:12 +0300

    xml

--- scrap-xml.py
+++ scrap-xml.py
@@ -1,4 +1,4 @@
-import urllib.request, urllib.parse, urllib.error
+import urllib.request,urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
 
@@ -22,22 +22,13 @@ while True:
     address = input('Enter location: ')
     if len(address) < 1: break
 
-    parms = dict()
-    parms['address'] = address
-    if api_key is not False: parms['key'] = api_key
-    url = serviceurl + urllib.parse.urlencode(parms)
-    print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
-
+    uh = urllib.request.urlopen(address)
     data = uh.read()
-    print('Retrieved', len(data), 'characters')
-    print(data.decode())
     tree = ET.fromstring(data)
+    counts = tree.findall('.//count')
 
-    results = tree.findall('result')
-    lat = results[0].find('geometry').find('location').find('lat').text
-    lng = results[0].find('geometry').find('location').find('lng').text
-    location = results[0].find('formatted_address').text
+    sum = 0
+    for each in counts:
+        sum += int(each.text)
 
-    print('lat', lat, 'lng', lng)
-    print(location)
+    print(sum)
